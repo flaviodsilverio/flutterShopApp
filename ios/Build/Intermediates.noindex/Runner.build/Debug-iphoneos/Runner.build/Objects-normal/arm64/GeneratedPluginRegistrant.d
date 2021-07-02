d3623464f72968181c2ef56c91d1d097f2e52cf8
Flavio Silverio <>
2021-07-02 17:53:19 +0200

    First commit

---
+++ ios/Build/Intermediates.noindex/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -0,0 +1,21 @@
+dependencies: \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Modules/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.4.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.4.sdk/usr/include/mach-o/dyld.modulemap \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Flutter/Flutter.framework/Headers/Flutter.h
---
+++ ios/Build/Intermediates.noindex/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/flavio.silverio/Desktop/flutter/shop_app/shop_app/ios/Build/Intermediates.noindex/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
